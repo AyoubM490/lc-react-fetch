33098754b65255fe7cf3a886ff04d41ec951be58
Andre Madarang <andre_madarang@hotmail.com>
2021-07-06 17:22:08 -0400

    Episode 13 - Fetching Data from external server

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Joke() {
-  return <div>Joke API result here</div>;
+export default function Reddit() {
+  const [joke, setJoke] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://official-joke-api.appspot.com/jokes/random')
+      .then(response => response.json())
+      .then(result => {
+        setIsLoading(false);
+        setJoke(result.setup + ' ' + result.punchline);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setErrorMessage('There was an error');
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Joke API</h2>
+      {isLoading && <div>Loading...</div>}
+      {joke && <div>{joke}</div>}
+      {errorMessage && <div>{errorMessage}</div>}
+    </div>
+  );
 }
--- src/Reddit.jsx
+++ src/Reddit.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Reddit() {
-  return <div>Reddit API results here</div>;
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/aww.json')
+      .then(response => response.json())
+      .then(results => {
+        // console.log(results.data.children);
+        setIsLoading(false);
+        setPosts(results.data.children);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setErrorMessage('There was an error');
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Reddit API</h2>
+      {isLoading && <div>Loading...</div>}
+      {posts && (
+        <ul>
+          {posts.map(post => (
+            <li key={post.data.id}>
+              <a href={`https://reddit.com${post.data.permalink}`}>
+                {post.data.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {errorMessage && <div>{errorMessage}</div>}
+    </div>
+  );
 }
